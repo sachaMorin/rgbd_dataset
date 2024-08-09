ddad27f6694292bdcf329910b621ed56432963a6
Morin Sacha <sacha.morin@nokia.com>
2024-08-09 11:16:44 -0400

    Create dir with to_nerfstudio

--- to_nerfstudio.py
+++ to_nerfstudio.py
@@ -1,3 +1,4 @@
+import os
 import hydra
 from omegaconf import DictConfig
 
@@ -5,6 +6,7 @@ from omegaconf import DictConfig
 @hydra.main(version_base=None, config_path="conf", config_name="to_nerfstudio")
 def main(cfg: DictConfig):
     dataset = hydra.utils.instantiate(cfg.dataset)
+    os.makedirs(cfg.dir, exist_ok=True)
     dataset.to_nerfstudio_config(cfg.dir)
 
 
