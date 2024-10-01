73f9522f406b7963ae26b4535591377c6247601b
sachaMorin <morin.sacha@gmail.com>
2024-10-01 08:39:06 -0400

    Mode to process all ProcTHOR scenes

--- rgbd_dataset/datasets/ProcTHOR.py
+++ rgbd_dataset/datasets/ProcTHOR.py
@@ -1,3 +1,4 @@
+import os
 from typing import Union
 from pathlib import Path
 import glob
@@ -13,35 +14,55 @@ from ..utils import invert_se3
 class ProcTHOR(BaseRGBDDataset):
     def __init__(self, split: str = "train", **kwargs):
         self.split = split
+        self.scene = kwargs["scene"]
+        self.base_path = Path(kwargs["base_path"])
+
+        if isinstance(self.scene, int):
+            self.scenes = [self.scene]
+        elif self.scene == "all":
+            self.scenes = natsorted(os.listdir(self.base_path / self.split ))
+        else:
+            raise ValueError("Invalide scene argument.")
+
         super().__init__(**kwargs)
 
     def get_rgb_paths(self) -> List[str]:
-        path_str = str(
-            self.base_path / self.split / str(self.scene) / "color_main" / "*.jpg"
-        )
-        rgb_paths = natsorted(glob.glob(path_str))
+        rgb_paths = []
+
+        for s in self.scenes:
+            path_str = str(
+                self.base_path / self.split / str(s) / "color_main" / "*.jpg"
+            )
+            rgb_paths += natsorted(glob.glob(path_str))
         return rgb_paths
 
     def get_depth_paths(self) -> List[str]:
-        path_str = str(
-            self.base_path / self.split / str(self.scene) / "depth_main" / "*.png"
-        )
-        depth_paths = natsorted(glob.glob(path_str))
+        depth_paths = []
+
+        for s in self.scenes:
+            path_str = str(
+                self.base_path / self.split / str(s) / "depth_main" / "*.png"
+            )
+            depth_paths += natsorted(glob.glob(path_str))
         return depth_paths
 
     def get_se3_poses(self) -> List[np.array]:
-        robot2cam_path = str(
-            self.base_path / self.split / str(self.scene) / "extrinsics.json"
-        )
-        robot2cam = np.array(
-            json.loads(open(robot2cam_path).read())["color_main"]
-        ).reshape(4, 4)
-        cam2robot = invert_se3(robot2cam)
+        pose_paths = []
 
-        pose_path = str(
-            self.base_path / self.split / str(self.scene) / "pose" / "*.json"
-        )
-        pose_paths = natsorted(glob.glob(pose_path))
+        for s in self.scenes:
+            robot2cam_path = str(
+                self.base_path / self.split / str(s) / "extrinsics.json"
+            )
+            robot2cam = np.array(
+                json.loads(open(robot2cam_path).read())["color_main"]
+            ).reshape(4, 4)
+            cam2robot = invert_se3(robot2cam)
+
+            pose_path = str(
+                self.base_path / self.split / str(s) / "pose" / "*.json"
+            )
+            pose_paths += natsorted(glob.glob(pose_path))
+
         poses = []
         for path in pose_paths:
             robot2world = json.loads(open(path).read())
